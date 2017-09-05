2d747645a8744bfc7dc7badf047b562e128d19e9
JP Juliao <jpjuliao@gmail.com>
2017-09-05 12:08:32 -0500

    Add files via upload

---
+++ bridge-child/functions.php
@@ -0,0 +1,26 @@
+<?php
+
+/**
+ * Child Style & Scripts
+ */
+
+function child_enqueue_scripts() {
+  wp_enqueue_style( 
+    'childstyle', 
+    get_stylesheet_directory_uri() . '/style.css'  
+  );
+}
+add_action( 'wp_enqueue_scripts', 'child_enqueue_scripts', 11);
+
+/**
+ * Unregister Bridge Post Types
+ */
+
+add_action('init', 'child_unregister_bridge_post_types',10);
+function child_unregister_bridge_post_types(){
+    unregister_post_type('portfolio_page');    
+    unregister_post_type('testimonials');    
+    unregister_post_type('masonry_gallery'); 
+    unregister_post_type('carousels');    
+    unregister_post_type('slides');    
+}
