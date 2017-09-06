ec393466cf0f787c2629dde4448bb348380b773b
JP Juliao <jpjuliao@gmail.com>
2017-09-06 00:14:19 -0500

    Update functions.php

--- bridge-child/functions.php
+++ bridge-child/functions.php
@@ -3,7 +3,6 @@
 /**
  * Child Style & Script
  */
-
 function child_enqueue_scripts() {
   wp_enqueue_style( 
     'childstyle', 
@@ -13,14 +12,39 @@ function child_enqueue_scripts() {
 add_action( 'wp_enqueue_scripts', 'child_enqueue_scripts', 11);
 
 /**
- * Unregister Bridge Post Types
+ * Unregister Post Types
  */
-
-add_action('init', 'child_unregister_bridge_post_types',10);
-function child_unregister_bridge_post_types(){
+function child_unregister_post_types(){
     unregister_post_type('portfolio_page');    
     unregister_post_type('testimonials');    
     unregister_post_type('masonry_gallery'); 
     unregister_post_type('carousels');    
     unregister_post_type('slides');    
 }
+add_action( 'init', 'child_unregister_post_types', 11 );
+
+/**
+ * Unregister Sidebars
+ */
+function child_unregister_sidebars(){
+    unregister_sidebar( 'sidebar_page' );
+    unregister_sidebar( 'header_left' );
+    unregister_sidebar( 'header_right' );
+    unregister_sidebar( 'header_left_from_logo' );
+    unregister_sidebar( 'header_right_from_logo' );
+    unregister_sidebar( 'header_bottom_right' );
+    unregister_sidebar( 'header_bottom_center' );
+    unregister_sidebar( 'sidearea' );
+    unregister_sidebar( 'fullscreen_menu_area_widget' );
+    unregister_sidebar( 'vertical_menu_area' );
+    unregister_sidebar( 'footer_column_1' );
+    unregister_sidebar( 'footer_column_2' );
+    unregister_sidebar( 'footer_column_3' );
+    unregister_sidebar( 'footer_column_4' );
+    unregister_sidebar( 'footer_text' );
+    unregister_sidebar( 'footer_text_left' );
+    unregister_sidebar( 'footer_text_right' );
+    unregister_sidebar( 'header_fixed_right' );
+    unregister_sidebar( 'woocommerce_dropdown' );
+}
+add_action( 'widgets_init', 'child_unregister_sidebars', 11 );
